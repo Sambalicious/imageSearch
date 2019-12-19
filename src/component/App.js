cc6502d760042f4098690a60b7ae8eb45480b1fe
samuel Temitope <stemitope370@gmail.com>
2019-12-19 12:10:54 +0100

    image search with grid display

---
+++ src/component/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList'
+
+
+
+class App extends React.Component {
+    state = {
+        images: []
+    };
+    onSearchSubmit = async term => { 
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term },
+            headers: {
+                Authorization:
+                 'Client-ID af275ff222e05bca5f4a32727bf4410ca4ee069e1f77bfe7c1b82c300ddad18a'
+                }  
+        });
+
+        this.setState({images: response.data.results});
+        
+    };
+    render(){
+return(
+<div className="ui container" style={{marginTop:'10px', body:'black'}}>
+<SearchBar onSubmit={this.onSearchSubmit} />
+<ImageList images={this.state.images} /> 
+</div>
+);
+}
+} 
+
+
+export default App;
---
+++ src/component/ImageList.js
@@ -0,0 +1,14 @@
+import './ImageList.css'
+import React from 'react'
+
+
+const ImageList = (props) => {
+   const images=  props.images.map((image)=>{
+        return <img key={image.id} src={image.urls.regular}  alt={image.description} />
+    })
+    return <div className="image-list">{images} </div>
+}
+
+
+
+export default ImageList;
