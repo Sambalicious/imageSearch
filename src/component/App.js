6b89dbe0edd4b55abc3f16fcb7b2e1ed90123c57
samuel Temitope <stemitope370@gmail.com>
2019-12-19 21:08:06 +0100

    the grid now aligns based on the image height automatically

--- src/component/App.js
+++ src/component/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     };
     render(){
 return(
-<div className="ui container" style={{marginTop:'10px', body:'black'}}>
+<div className="ui container" style={{marginTop:'10px'}}>
 <SearchBar onSubmit={this.onSearchSubmit} />
 <ImageList images={this.state.images} /> 
 </div>
--- src/component/ImageList.js
+++ src/component/ImageList.js
@@ -1,10 +1,12 @@
-import './ImageList.css'
-import React from 'react'
+import './ImageList.css';
+import React from 'react';
+import ImageCard from './imageCard';
+
 
 
 const ImageList = (props) => {
    const images=  props.images.map((image)=>{
-        return <img key={image.id} src={image.urls.regular}  alt={image.description} />
+return <ImageCard key={image.id} image={image} />
     })
     return <div className="image-list">{images} </div>
 }
